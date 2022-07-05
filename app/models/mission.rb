95a9a657e970a02ae2676681478a1afe92cdcb3d
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-05 11:09:07 -0600

    all done and fixed

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,6 +2,6 @@ class Mission < ApplicationRecord
   belongs_to :scientist
   belongs_to :planet
 
-  validates :name, :scientist_id, :planet_id, presence: true
-  validates :scientist_id, uniqueness: true
+  validates :name, presence: true
+  validates :scientist_id, uniqueness: {scope: :planet_id}
 end
